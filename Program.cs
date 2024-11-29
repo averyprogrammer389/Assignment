90663940e1758fe348f5ac0691d2b2b9b80b6e6a
averyprogrammer389 <avery_jones_programmer@outlook.com>
2024-11-29 14:18:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+            {
+                stringBuilder.Append(rand.Next(1, 6));
+            }
+
+            // string generatedNum = stringBuilder.ToString();
+
+
+            string generatedNum = "1234";
+            //string guess = "4233";
+            string guess = string.Empty;
+
+            int counter = 0;
+            string output = string.Empty;
+            while (counter < 10)
+            {
+                Console.WriteLine("Please enter a number guess");
+                guess = Console.ReadLine();
+                for (int k = 0; k < generatedNum.Length; k++)
+                {
+                    if (generatedNum[k] == guess[k])
+                    {
+                        output += "+";
+                        continue;
+                    }
+                    else if (generatedNum[k] != guess[k] && guess.Contains(generatedNum[k]))
+                    {
+                        output += "-";
+                        continue;
+                    }
+
+                    if (output.Count(x => x == '+') == 4)
+                    {
+                        Console.WriteLine("Number Guessed");
+                        break;
+                    }
+
+                    continue;
+
+
+                }
+                counter++;
+            }
+
+        }
+    }
+}
+
